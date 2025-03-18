dff8d1c7c85ce568083e073c8efc1fc18f523e08
FillerDK <fillerdk1234@gmail.com>
2025-03-18 17:05:59 +0100

    Gui class created, output to server now works again

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,29 +1,26 @@
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
 
 import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-public class App extends Application {
+public class App{
 
-    private final Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
-    private final int WINDOW_WIDTH = (int) screenBounds.getWidth();
-    private final int WINDOW_HEIGHT = (int) screenBounds.getHeight();
-
-    private final GameRenderer gameRenderer = new GameRenderer(WINDOW_WIDTH, WINDOW_HEIGHT);
+    public static DataOutputStream outToServer = null;
 
     public static void main(String[] args) {
 
         // MAIN THREAD
         String name = "Mads";
         Socket clientSocket;
-        DataOutputStream outToServer = null;
         RecieverThread recieverThread;
 
         try {
@@ -42,43 +39,7 @@ public class App extends Application {
         }
 
         SpriteLoader.loadSprites();
-        launch(args);
+        Application.launch(Gui.class);
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        BorderPane root = new BorderPane();
-
-        // Stack the debug overlay on top of main application
-        StackPane mainPane = new StackPane(gameRenderer);
-
-        mainPane.setMaxSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-        mainPane.setMinSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-        mainPane.setPrefSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-
-        root.setCenter(mainPane);
-
-        root.setStyle("-fx-background-color: #f0f0f0;");
-
-        Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
-
-        // Set background color for the scene
-        scene.setFill(javafx.scene.paint.Color.LIGHTGRAY);
-
-        primaryStage.setScene(scene);
-        primaryStage.setResizable(false);
-        primaryStage.setTitle("THE NETWORK GAME CLIENT");
-
-        toggleWindowedFullscreen(primaryStage);
-
-        primaryStage.show();
-    }
-
-    private void toggleWindowedFullscreen(Stage primaryStage) {
-        primaryStage.setX(screenBounds.getMinX());
-        primaryStage.setY(screenBounds.getMinY());
-        primaryStage.setWidth(screenBounds.getWidth());
-        primaryStage.setHeight(screenBounds.getHeight());
-        primaryStage.initStyle(StageStyle.UNDECORATED);
-    }
 }
